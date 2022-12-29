37b604db23432c5014aef7383a4108f49f72d291
Gil <frogvsdogvsmarsmelo@gmail.com>
2022-12-29 16:42:24 +0200

    Started to work on home page

---
+++ frontend/src/products.js
@@ -0,0 +1,29 @@
+const products = [
+    {
+        _id: '1',
+        name: 'Red surfboard',
+        image: '/images/GoFish_Red_1500px_740x.webp',
+        description:
+            "Every surfer needs a surfboard that replicates the flow, the style, and the smooth glide of a classical template surfboard. A fish surfboard is a perfect choice, as these retro-inspired shapes bring you right on back to the good ol' days of surfing.",
+        brand: 'Go Fish',
+        category: 'SurfBoards',
+        price: 79.99,
+        unitsInStock: 5,
+        rating: 4.6,
+        numReviews: 15
+    },
+    {
+        _id: '2',
+        name: 'Old School Surfboard',
+        image: '/images/istockphoto-941325138-612x612.jpg',
+        description:
+            "Softech Lil' Ripper Surfboard",
+        brand: 'Surfdome',
+        category: 'SurfBoards',
+        price: 120,
+        unitsInStock: 10,
+        rating: 4.8,
+        numReviews: 19
+    },
+
+]
