f30540ee545b28ce5f5e56194b125a128774e6d1
Gil <frogvsdogvsmarsmelo@gmail.com>
2023-01-01 19:37:11 +0200

    Added env file and changed the type to module

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
-const express = require('express')
-const products = require('./data/products')
+import express from 'express'
+import dotenv from 'dotenv'
+import products from './data/products.js'
+dotenv.config()
 const app = express()
 
 app.get('/api/products', (req, res) => {
@@ -9,4 +11,5 @@ app.get('/api/products/:id', (req, res) => {
     const product = products.find(p => p._id === req.params.id)
     res.json(product)
 })
-app.listen(5000, console.log('Server running on port 5000'))
+const PORT = process.env.PORT || 5000
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
