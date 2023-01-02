33553a246ad89c2ab146b7a5cf0fc45bdc688c96
Gil <frogvsdogvsmarsmelo@gmail.com>
2023-01-02 21:59:06 +0200

    Started to work on the cart screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
+import CartScreen from "./screens/CartScreen";
 const App = () => {
   return (
     <Router>
@@ -14,6 +15,8 @@ const App = () => {
           <Routes>
             <Route path='/' element={<HomeScreen />} exact />
             <Route path='/product/:id' element={<ProductScreen />} />
+            <Route path='/cart/:id?' element={<CartScreen />} />
+
           </Routes>
 
         </Container>
