f16c3b51e89117d7a0f278455699ca1acc8fbb9c
Gil <frogvsdogvsmarsmelo@gmail.com>
2023-01-03 22:38:56 +0200

    Added all the backend authentication

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,9 +1,7 @@
 import express from 'express'
 const router = express.Router()
 import { getProducts, getProductById } from '../controllers/productController.js'
-// @desc   Fetch all products
-// @route  GET /api/products
-// @access Public
+
 router.route('/').get(getProducts)
 router.route('/:id').get(getProductById)
 
