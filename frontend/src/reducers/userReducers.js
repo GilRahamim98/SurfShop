6f008ef0eb32fb96b49a20e906bf6534e84cce8b
Gil <frogvsdogvsmarsmelo@gmail.com>
2023-01-04 11:37:26 +0200

    Added profile screen with update profile option

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -5,6 +5,13 @@ import {
     USER_REGISTER_FAIL,
     USER_REGISTER_REQUEST,
     USER_REGISTER_SUCCESS,
+    USER_DETAILS_FAIL,
+    USER_DETAILS_REQUEST,
+    USER_DETAILS_SUCCESS,
+    USER_UPDATE_PROFILE_FAIL,
+    USER_UPDATE_PROFILE_REQUEST,
+    USER_UPDATE_PROFILE_RESET,
+    USER_UPDATE_PROFILE_SUCCESS,
     USER_LOGOUT
 } from '../constants/userConstants'
 
@@ -22,7 +29,6 @@ export const userLoginReducer = (state = {}, action) => {
         default:
             return state
     }
-
 }
 
 export const userRegisterReducer = (state = {}, action) => {
@@ -36,5 +42,30 @@ export const userRegisterReducer = (state = {}, action) => {
         default:
             return state
     }
+}
 
+export const userDetailsReducer = (state = { user: {} }, action) => {
+    switch (action.type) {
+        case USER_DETAILS_REQUEST:
+            return { ...state, loading: true }
+        case USER_DETAILS_SUCCESS:
+            return { loading: false, user: action.payload }
+        case USER_DETAILS_FAIL:
+            return { loading: false, error: action.payload }
+        default:
+            return state
+    }
+}
+
+export const userUpdateProfileReducer = (state = {}, action) => {
+    switch (action.type) {
+        case USER_UPDATE_PROFILE_REQUEST:
+            return { loading: true }
+        case USER_UPDATE_PROFILE_SUCCESS:
+            return { loading: false, success: true, userInfo: action.payload }
+        case USER_UPDATE_PROFILE_FAIL:
+            return { loading: false, error: action.payload }
+        default:
+            return state
+    }
 }
