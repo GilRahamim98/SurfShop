f61c3e02502c31021ab2a2f5abb9aed90cfaeda8
Gil <frogvsdogvsmarsmelo@gmail.com>
2023-01-08 14:36:17 +0200

    Added review endpoint in the backend

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -14,6 +14,12 @@ const reviewSchema = mongoose.Schema({
         type: String,
         required: true
     },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'User'
+
+    },
 }, {
     timestamps: true
 
