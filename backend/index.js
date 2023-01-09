147da93b7a5365452a453de73f2a64e5f0539e1a
Gil <frogvsdogvsmarsmelo@gmail.com>
2023-01-09 13:47:52 +0200

    Changed to app to production mode

--- backend/index.js
+++ backend/index.js
@@ -24,8 +24,15 @@ app.get('/api/config/paypal', (req, res) => res.send(process.env.PAYPAL_CLIENT_I
 
 const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/frontend/build')))
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')))
+}
+
 app.use(notFound)
 app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
+
